fcf2ee32c79a70fea2c5fdb3f045454a26b17c09
Zehra Karakas <zehra.karakas@goflink.com>
2022-11-16 22:03:41 +0300

    Fetch quiz questions

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { BASE_URL } from "../constants";
+
 export const Quiz = () => {
+  const { id } = useParams();
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    const getQuiz = async () => {
+      const response = await fetch(`${BASE_URL}/quiz/${id}`).then((response) =>
+        response.json()
+      );
+
+      setQuestions(response.questions);
+
+      console.log(response.questions);
+    };
+
+    getQuiz();
+  }, []);
+
   return <div>QUIZ</div>;
 };
